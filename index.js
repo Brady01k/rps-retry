31f6df94a6db672ccce6ddd9a0a9e6d8e6dd6bf7
Brady01k <bradykillham01@gmail.com>
2022-11-24 16:22:59 -0600

    Create function to play one round
    create prompt at beginning of function for use to give input

--- index.js
+++ index.js
@@ -1,9 +1,28 @@
 // array containing options for computer to choose
 let options = ["rock", "paper", "scissors"]
 // function for computer to choose a random value from array
-function getComputerChoice() {
+function getComputerChoice(options) {
     let choice = options[(Math.random * options.length) | 0]
     return choice
 }
+//variables to hold scores
+playerScore = 0
+computerScore = 0
 //function to take user input and play one round
+function playRound() {
+    let playerChoice = prompt("Please choose rock, paper, or scissors: ").toLowerCase()
+    let computerChoice = getComputerChoice()
+    
+    if (playerChoice === computerChoice ) {
+        return tie
+    }
+    else if (playerChoice === "rock" && computerChoice === "scissors" || playerChoice === "paper" && computerChoice === "rock" || playerChoice === "scissors" && computerChoice === "paper") {
+        playerScore++
+        return win
+    }
+    else if (playerChoice === "rock" && computerChoice === "paper" || playerChoice === "paper" && computerChoice === "scissors" || playerChoice === "scissors" && computerChoice === "rock") {
+        computerScore++
+        return lose
+    }
+}
 //function to play the game for 5 rounds and keep score
